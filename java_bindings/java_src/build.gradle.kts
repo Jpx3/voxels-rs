933036936a9e96a6231109feb7421a1c8eddffdf
Richy <strunk876@gmail.com>
2026-02-09 21:10:59 +0100

    Update signing configuration in Maven publish workflow

--- java_bindings/java_src/build.gradle.kts
+++ java_bindings/java_src/build.gradle.kts
@@ -1,82 +1,74 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    `java-library`
-    id("com.vanniktech.maven.publish") version "0.28.0"
+  `java-library`
+  id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 tasks.register<Exec>("buildRust") {
-    workingDir = file("..")
-    commandLine("cargo", "build", "--release")
+  workingDir = file("..")
+  commandLine("cargo", "build", "--release")
 }
 
 tasks.named<ProcessResources>("processResources") {
-    if (System.getenv("CI") == null) {
-        dependsOn("buildRust")
-        from("../../target/release") {
-            include("*.dll", "*.so", "*.dylib")
-            into("native")
-        }
+  if (System.getenv("CI") == null) {
+    dependsOn("buildRust")
+    from("../../target/release") {
+      include("*.dll", "*.so", "*.dylib")
+      into("native")
     }
-}
-
-signing {
-  val signingKey = project.findProperty("signingKey") as String?
-  val signingPassword = project.findProperty("signingPassword") as String?
-
-  useInMemoryPgpKeys(signingKey, signingPassword)
-  sign(publishing.publications["mavenJava"])
+  }
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+  publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-    signAllPublications()
+  signAllPublications()
 
-    coordinates("de.richy", "voxels_rs", "0.1.8")
+  coordinates("de.richy", "voxels_rs", "0.1.8")
 
-    pom {
-        name.set("voxels_rs")
-        description.set("Java bindings for the voxels_rs library")
-        inceptionYear.set("2026")
-        url.set("https://github.com/Jpx3/voxels_rs")
+  pom {
+    name.set("voxels_rs")
+    description.set("Java bindings for the voxels_rs library")
+    inceptionYear.set("2026")
+    url.set("https://github.com/Jpx3/voxels_rs")
 
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("Jpx3")
-                name.set("Richy")
-                url.set("https://github.com/Jpx3")
-            }
-        }
-
-        scm {
-            url.set("https://github.com/Jpx3/voxels_rs")
-            connection.set("scm:git:git://github.com/Jpx3/voxels_rs.git")
-            developerConnection.set("scm:git:ssh://git@github.com/Jpx3/voxels_rs.git")
-        }
+    licenses {
+      license {
+        name.set("The Apache License, Version 2.0")
+        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+      }
     }
+
+    developers {
+      developer {
+        id.set("Jpx3")
+        name.set("Richy")
+        url.set("https://github.com/Jpx3")
+      }
+    }
+
+    scm {
+      url.set("https://github.com/Jpx3/voxels_rs")
+      connection.set("scm:git:git://github.com/Jpx3/voxels_rs.git")
+      developerConnection.set("scm:git:ssh://git@github.com/Jpx3/voxels_rs.git")
+    }
+  }
 }
 
 dependencies {
-    testImplementation(libs.junit)
-    api(libs.commons.math3)
-    implementation(libs.guava)
+  testImplementation(libs.junit)
+  api(libs.commons.math3)
+  implementation(libs.guava)
 }
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
